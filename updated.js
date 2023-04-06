14d143a655c57d3fbb1e00ea00b06d662aa28a29
reiquel <carelary92@gmail.com>
2023-04-06 01:15:05 -0400

    afaf

--- updated.js
+++ updated.js
@@ -1,5 +1,15 @@
+const opciones2 = 
+{weekday: 'long', 
+year: 'numeric', 
+month: 'long', 
+day: 'numeric',
+hour: '2-digit',
+minute: '2-digit',
+second: '2-digit' };
 
 
-const lastModified = new Date(document.lastModified);
-const dateMod = document.getElementById("dateMod");
-dateMod.innerHTML = lastModified.toDateString();
+
+const fecha2 = new Date();
+const fechaFormateada2 = fecha2.toLocaleString('en-UK', opciones2);
+
+document.getElementById('dateMod').textContent = fechaFormateada2;
