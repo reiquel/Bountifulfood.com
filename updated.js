e019427c34a3aa920d154ec7420cfe4b4b22322b
reiquel <carelary92@gmail.com>
2023-04-05 23:31:45 -0400

    afaf

--- updated.js
+++ updated.js
@@ -1,12 +1,5 @@
 
-const opciones2 = 
-{ weekday: 'long', 
-year: 'numeric', 
-month: 'long', 
-day: 'numeric' };
 
-
-document.querySelector('#dateMod').textContent = 
-new Date(Date.now()).toLocaleString('en-UK',opciones2);
-
-
+const lastModified = new Date(document.lastModified);
+const dateMod = document.getElementById("dateMod");
+dateMod.innerHTML = lastModified.toDateString();
